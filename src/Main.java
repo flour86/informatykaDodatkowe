1bf7851b840fb68097feccdda09c5f4a52ac6671
BuildTools <unconfigured@null.spigotmc.org>
2021-12-09 21:10:20 +0100

    9.12.21

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,76 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean loop1 = true;
+        boolean loop = true;
 
-        while (loop1) {
+        while (loop) {
             System.out.println("---------Gry oparte na RNG---------");
             System.out.println();
             System.out.println("Co chcesz zrobić?");
             System.out.println("1 aby zagrac w  papier, kamien i nozyce");
             System.out.println("2 aby zagrac w kosci");
-            System.out.println("3 aby uzyc randomizera okreslonych rzeczy");
+            System.out.println("3 aby zagrac w gre z kulkami ze \"Squid Game\"");
+            System.out.println("4 aby uzyc randomizera okreslonych rzeczy");
+            System.out.println();
+            System.out.println("0 aby wyjsc");
             System.out.println();
 
             Scanner scan1 = new Scanner(System.in);
             int scan1Int = scan1.nextInt();
 
+            if (scan1Int == 0) {
+                loop = false;
+            }
+
             switch (scan1Int) {
                 case 1:
                     PapierKamienNozyce();
+                    break;
+
+                case 2:
+                    Kosci();
+                    break;
+
+                case 3:
+                    squidGameKulki();
+                    break;
+
+                case 4:
+                    wheelDecideRipOff();
+                    break;
+
+                case 0:
+                    loop = false;
+                    break;
             }
         }
     }
 
     public static void PapierKamienNozyce() {
-        Random rand1 = new Random();
+        Random rand = new Random();
         int playerScore = 0;
         int enemyScore = 0;
-        boolean loop2 = true;
+        int loop = 0;
 
-        while (loop2 ) {
+        while (loop < 3) {
+            System.out.println();
             System.out.println("Witaj w grze papier, kamien nozyce!");
             System.out.println();
-            System.out.println("Wynik: grasz - " + playerScore + "; a CPU - " + enemyScore);
+            System.out.println("Wynik: gracz - " + playerScore + "; a CPU - " + enemyScore);
             System.out.println("Czym chcesz zagrac?");
             System.out.println("1 - Papier");
             System.out.println("2 - Kamien");
             System.out.println("3 - Nozyce");
             System.out.println();
+            System.out.println("0 - Wyjscie");
+            System.out.println();
 
             Scanner scan = new Scanner(System.in);
             int scanInt = scan.nextInt();
-            int enemyPick = rand1.nextInt(2);
+            int enemyPick = rand.nextInt(3);
 
             switch (scanInt) {
                 case 1:
@@ -81,18 +109,109 @@ public class Main {
                     }
                     break;
 
+                case 0:
+                    loop = 4;
+                    break;
+
                 default:
                     break;
             }
-            System.out.println("Czy chcesz powtorzyc gre w papier kamien i nozyce? Y/N");
-            Scanner scan2 = new Scanner(System.in);
-            String scan2String = scan.next();
-            if ( scan2String == "Y" || scan2String == "y" ) {
-                loop2 = true;
+            loop++;
+        }
+    }
+
+    public static void Kosci() {
+        Random rand = new Random();
+        int playerScore = 0;
+        int enemyScore = 0;
+        int loop = 0;
+
+        while (loop < 3) {
+            System.out.println();
+            System.out.println("Witaj w grze w Kosci!");
+            System.out.println();
+            System.out.println("Wynik: gracz - " + playerScore + "; a CPU - " + enemyScore);
+            System.out.println("Na ile kostek chcesz grac? (mozna podac duza liczbe, ale tego nie radze)");
+            System.out.println();
+            System.out.println("Exit - wyjscie (nie dziala, trzeba przeklikac sie przez gre )");
+            System.out.println();
+
+            Scanner scan = new Scanner(System.in);
+            int scanInt = scan.nextInt();
+            int diceNum = scanInt;
+
+            int dicePossibleOutcome = 6 * diceNum;
+
+            int playerDice = rand.nextInt(dicePossibleOutcome);
+            int enemyDice = rand.nextInt(dicePossibleOutcome);
+
+            System.out.println("Kosci gracza: " + playerDice);
+            System.out.println("Kosci CPU: " + enemyDice);
+            System.out.println();
+
+            if (playerDice > enemyDice) {
+                System.out.println("Wygrywasz!");
+                playerScore++;
+            }
+            else if(enemyDice > playerDice) {
+                System.out.println("Przegrywasz...");
+                enemyScore++;
             }
             else {
-                loop2 = false;
+                System.out.println("Remis");
             }
+            loop++;
         }
+    }
+
+    public static void squidGameKulki() {
+        Random rand = new Random();
+        int playerScore = 0;
+        int enemyScore = 0;
+        int loop = 0;
+
+        while (loop < 3) {
+            System.out.println();
+            System.out.println("Witaj w slabej wersji gry z kulkami ze \"Squid Game\"");
+            System.out.println();
+            System.out.println("Jaki budzet kulek chcesz miec?");
+            System.out.println();
+            System.out.println("Exit - wyjscie (nie dziala, trzeba przeklikac sie przez gre )");
+            System.out.println();
+
+            Scanner scan = new Scanner(System.in);
+            int scanInt = scan.nextInt();
+            int marbleBudget = scanInt;
+
+            int playerMarbles = marbleBudget;
+            int enemyMarbles = marbleBudget;
+
+            System.out.println("Obstawiasz parzystste (true), czy nieparzyste (false)?");
+            System.out.println();
+            Scanner guess = new Scanner(System.in);
+            boolean playerGuess = guess.hasNextBoolean();
+
+            if (enemyMarbleCountIsEven(enemyMarbles) == playerGuess) {
+                //kulki od oponenta leca do gracza, cpu traci kulki ktore zbidowal
+            }
+            else {
+                //obstawienie gracza leci do oponenta
+            }
+
+            loop++;
+        }
+    }
+
+    public static boolean enemyMarbleCountIsEven(int enemyMarbles) {
+        if (enemyMarbles % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void wheelDecideRipOff() {
+
     }
 }
