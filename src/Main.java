1219fbbdbd0bbacf131d858409aaf6ffae595f3b
BuildTools <unconfigured@null.spigotmc.org>
2021-12-11 19:11:39 +0100

    11.12.21 19:10

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main {
             System.out.println("1 aby zagrac w  papier, kamien i nozyce");
             System.out.println("2 aby zagrac w kosci");
             System.out.println("3 aby zagrac w gre z kulkami ze \"Squid Game\"");
-            System.out.println("4 aby uzyc randomizera okreslonych rzeczy");
             System.out.println();
             System.out.println("0 aby wyjsc");
 
@@ -26,25 +25,10 @@ public class Main {
             }
 
             switch (scan1Int) {
-                case 1:
-                    PapierKamienNozyce();
-                    break;
-
-                case 2:
-                    Kosci();
-                    break;
-
-                case 3:
-                    squidGameKulki();
-                    break;
-
-                case 4:
-                    wheelDecideRipOff();
-                    break;
-
-                case 0:
-                    loop = false;
-                    break;
+                case 1 -> PapierKamienNozyce();
+                case 2 -> Kosci();
+                case 3 -> squidGameKulki();
+                case 0 -> loop = false;
             }
         }
     }
@@ -53,9 +37,9 @@ public class Main {
         Random rand = new Random();
         int playerScore = 0;
         int enemyScore = 0;
-        int loop = 0;
+        boolean loop = true;
 
-        while (loop < 3) {
+        while (loop) {
             System.out.println();
             System.out.println("Witaj w grze papier, kamien nozyce!");
             System.out.println();
@@ -109,13 +93,9 @@ public class Main {
                     break;
 
                 case 0:
-                    loop = 4;
-                    break;
-
-                default:
+                    loop = false;
                     break;
             }
-            loop++;
         }
     }
 
@@ -123,41 +103,43 @@ public class Main {
         Random rand = new Random();
         int playerScore = 0;
         int enemyScore = 0;
-        int loop = 0;
+        boolean loop = true;
 
-        while (loop < 3) {
+        while (loop) {
             System.out.println();
             System.out.println("Witaj w grze w Kosci!");
             System.out.println();
             System.out.println("Wynik: gracz - " + playerScore + "; a CPU - " + enemyScore);
             System.out.println("Na ile kostek chcesz grac? (mozna podac duza liczbe, ale tego nie radze)");
             System.out.println();
-            System.out.println("Exit - wyjscie (nie dziala, trzeba przeklikac sie przez gre )");
+            System.out.println("0 - wyjscie");
 
             Scanner scan = new Scanner(System.in);
             int scanInt = scan.nextInt();
             int diceNum = scanInt;
 
-            int dicePossibleOutcome = 6 * diceNum;
+            if(scanInt != 0) {
+                int dicePossibleOutcome = 6 * diceNum;
 
-            int playerDice = rand.nextInt(dicePossibleOutcome);
-            int enemyDice = rand.nextInt(dicePossibleOutcome);
+                int playerDice = rand.nextInt(dicePossibleOutcome);
+                int enemyDice = rand.nextInt(dicePossibleOutcome);
 
-            System.out.println("Kosci gracza: " + playerDice);
-            System.out.println("Kosci CPU: " + enemyDice);
+                System.out.println("Kosci gracza: " + playerDice);
+                System.out.println("Kosci CPU: " + enemyDice);
 
-            if (playerDice > enemyDice) {
-                System.out.println("Wygrywasz!");
-                playerScore++;
-            }
-            else if(enemyDice > playerDice) {
-                System.out.println("Przegrywasz...");
-                enemyScore++;
+                if (playerDice > enemyDice) {
+                    System.out.println("Wygrywasz!");
+                    playerScore++;
+                } else if (enemyDice > playerDice) {
+                    System.out.println("Przegrywasz...");
+                    enemyScore++;
+                } else {
+                    System.out.println("Remis");
+                }
             }
             else {
-                System.out.println("Remis");
+                loop = false;
             }
-            loop++;
         }
     }
 
@@ -175,59 +157,54 @@ public class Main {
             System.out.println();
             System.out.println("Jaki budzet kulek chcesz miec?");
             System.out.println();
-            System.out.println("0 - wyjscie (nie dziala, trzeba przeklikac sie przez gre )");
+            System.out.println("0 - wyjscie");
 
             Scanner scan = new Scanner(System.in);
             int marbleBudget = scan.nextInt();
             playerMarbles = marbleBudget;
             enemyMarbles = marbleBudget;
 
-            while ((playerMarbles < marbleBudget * 2) || (enemyMarbles < marbleBudget * 2)) {
-                System.out.println("Masz " + playerMarbles + " kulek");
-                System.out.println("Ile kulek obstawiasz?");
-                Scanner bid = new Scanner(System.in);
-                int playerBid = bid.nextInt();
-                int enemyBid = ThreadLocalRandom.current().nextInt(1, marbleBudget + 1);
-
-                System.out.println("Obstawiasz parzystste (true), czy nieparzyste (false)?");
-                System.out.println(enemyBid); //test
-                Scanner guess = new Scanner(System.in);
-                boolean playerGuess = guess.hasNextBoolean();
-
-                if(playerBid > playerMarbles || playerBid < 1) {
-                    System.out.println("ERROR");
-                }
-                else {
-                    //cos jest z warunkiem nie tak
-                    System.out.println(enemyBidIsEven(enemyBid));//test
-                    if (playerGuess == enemyBidIsEven(enemyBid)) {
-                        playerMarbles = playerMarbles + enemyBid;
-                        enemyMarbles = enemyMarbles - enemyBid;
-                        System.out.println("Trafiles! Zyskujesz kulki przeciwnika");
-                        System.out.println(enemyMarbles);//test
+            if (marbleBudget != 0) {
+                while ((playerMarbles < marbleBudget * 2) && (enemyMarbles < marbleBudget * 2)) {
+                    System.out.println("Masz " + playerMarbles + " kulek");
+                    System.out.println("Ile kulek obstawiasz?");
+                    Scanner bid = new Scanner(System.in);
+                    int playerBid = bid.nextInt();
+                    int enemyBid;
+                    if (enemyMarbles > 1) {
+                        enemyBid = ThreadLocalRandom.current().nextInt(1, enemyMarbles);
                     } else {
-                        enemyMarbles = enemyMarbles + playerBid;
-                        playerMarbles = playerMarbles - playerBid;
-                        System.out.println("Przeciwnik cie przechytrzyl! Tracisz obstawione kulki...");
-                        System.out.println(enemyMarbles);//test
+                        enemyBid = 1;
+                    }
+
+                    int enemyBidEven;
+                    if (enemyBid % 2 == 1) {
+                        enemyBidEven = 0;
+                    } else {
+                        enemyBidEven = 1;
+                    }
+
+                    System.out.println("Obstawiasz parzystste (1), czy nieparzyste (0)?");
+                    Scanner guess = new Scanner(System.in);
+                    int playerGuess = guess.nextInt(); //chcialem to zrobic na booleanach, ale wtedy nie wiem czemu nie dzialalo, mimo, ze zmienilem wejscie i sprawdzenie na boolean
+
+                    if (playerBid > playerMarbles || playerBid < 1) {
+                        System.out.println("ERROR");
+                    } else {
+                        if (playerGuess == enemyBidEven) {
+                            playerMarbles = playerMarbles + enemyBid;
+                            enemyMarbles = enemyMarbles - enemyBid;
+                            System.out.println("Trafiles - zyskujesz kulki przeciwnika");
+                        } else {
+                            enemyMarbles = enemyMarbles + playerBid;
+                            playerMarbles = playerMarbles - playerBid;
+                            System.out.println("Przegrywasz - tracisz obstawione kulki...");
+                        }
                     }
                 }
+            } else {
+                loop = false;
             }
-            //wyjscie zrob, tak zeby dzialalo
         }
-    }
-
-    public static boolean enemyBidIsEven(int enemyBid) {
-        //mozliwe ze tutaj cos tez jest nie tak, ale raczej nie
-        if (enemyBid % 2 == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public static void wheelDecideRipOff() {
-        //wstawiasz slowa do tabeli stringow a pozniej losowo wybierasz komorke z tej tabeli i ja wyswietlasz
     }
 }
